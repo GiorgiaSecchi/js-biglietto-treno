adbfad691380121415260d47fcd67fa8adfaeb62
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-14 15:23:21 +0200

    procedura

--- js/main.js
+++ js/main.js
@@ -7,4 +7,31 @@
 // * - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // * - va applicato uno sconto del 20% per i minorenni
 // * - va applicato uno sconto del 40% per gli over 65.
-// * - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// * - L'output del prezzo finale va messo fuori in forma umana
+// *   (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// # PROCEDURA
+
+// ! RACCOLTA DATI
+
+// * chiedere all'utente il numero di chilometri che vuole percorrere
+// * controllo che sia valido
+// * chiedere all'utente l'età del passeggero
+// * controllo che sia valido
+
+// ! ELABORAZIONE
+
+// * SE (numero km utente) AND (l'età del passeggero) sono validi
+// *    calcolo in base a (numero km utente) il (prezzo km del biglietto)
+// *    SE (l'età del passeggero) è minore OR uguale a 17
+// *    applico sconto del 20% al (prezzo km del biglietto)
+// *    SE (l'età del passeggero) è maggiore OR uguale di 65
+// *    applico sconto del 40% al (prezzo km del biglietto)
+// *    ALTRIMENTI SE (l'età del passeggero) è un numero compreso fra 18 AND 64
+// *    preparo messaggio con (prezzo km del biglietto)
+// * ALTRIMENTI
+// *    apparirà messaggio di errore
+
+// ! OUTPUT
+
+// * stampo il messaggio che comunica il prezzo finale
